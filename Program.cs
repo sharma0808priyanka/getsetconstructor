367e8ed738491ed128c176633f407bec6ebbcd08
Priyanka Sharma <71631595+sharma0808priyanka@users.noreply.github.com>
2021-04-02 20:19:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace constructor_get_set
+{
+    class Book
+    {
+
+        int isbn;
+        int pages;
+        double price;
+        public Book() //constructor
+        {
+
+            this.isbn = -10; // default values
+            this.pages = -10;
+            this.price = -10;
+        }
+        public int getisbn() // get method
+        {
+            return this.isbn;
+        }
+        public int getpages()
+        {
+            return this.pages;
+        }
+        public double getprice()
+        {
+            return this.price;
+        }
+        public void setisbn(int myisbn) //set method
+        {
+            this.isbn = myisbn;
+        }
+        public void setpages(int mypages)
+        {
+            this.pages = mypages;
+        }
+        public void setprice(double myprice)
+        {
+            this.price = myprice;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            //b.setisbn(100); // inti values
+            //b.setpages(500);
+            //b.setprice(.00);
+            Console.WriteLine(b.getisbn());
+            Console.WriteLine(b.getpages());
+            Console.WriteLine(b.getprice());
+        }
+    }
+}
